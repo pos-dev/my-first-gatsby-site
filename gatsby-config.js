dad1416bdeecb2f5378da7d97c0a6244de49d9e4
Poese Toomata <toomatapoese@gmail.com>
2024-03-19 15:25:32 +0800

    finished part 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,18 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `My Gatsby Site`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `Pos Gatsby Site`
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+    "gatsby-plugin-mdx",
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,20 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
+import Seo from '../components/seo'
 
 // Step 2: Define your component
-const IndexPage = () => {
+function IndexPage() {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
-      <StaticImage alt="testing image" src="../images/icon.png"/>
+      <StaticImage alt="testing image" src="../images/icon.png" />
     </Layout>
   )
 }
 
 // You'll learn about this in the next task, just copy it for now
-export const Head = () => <title>Home Page</title>
+export const Head = () => <Seo title="Home Page" />
 
 // Step 3: Export your component
 export default IndexPage
